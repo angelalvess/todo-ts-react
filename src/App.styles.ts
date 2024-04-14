6b5286b6a5094d687ed8f68a3d7b4fc7f12317c2
angelalvess <angeltrashangel@gmail.com>
2024-04-14 13:48:43 -0300

    init project

---
+++ src/App.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #17181f;
+  color: #797a81;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 10px;
+`;
+
+export const Header = styled.h1`
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Container, Area, Header } from './App.styles';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Area>
+          <Header>Todo List</Header>
+        </Area>
+      </Container>
+    </>
+  );
+}
+
+export default App;
